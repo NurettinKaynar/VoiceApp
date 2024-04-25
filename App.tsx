f230d49cf55828864fc76ae4aa118b2b055f46d3
NurettinKaynar <nurettinkaynar568@gmail.com>
2024-04-25 15:22:26 +0300

    deneme Voice & recorderApp

--- App.tsx
+++ App.tsx
@@ -1,118 +1,565 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import AudioRecorderPlayer, {
+  AVEncoderAudioQualityIOSType,
+  AVEncodingOption,
+  AudioEncoderAndroidType,
+  AudioSourceAndroidType,
+  OutputFormatAndroidType,
+} from 'react-native-audio-recorder-player';
+import type {
+  AudioSet,
+  PlayBackType,
+  RecordBackType,
+} from 'react-native-audio-recorder-player';
 import {
-  SafeAreaView,
   ScrollView,
-  StatusBar,
+  Dimensions,
+  PermissionsAndroid,
+  Platform,
+  SafeAreaView,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import React, {Component} from 'react';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Button from './components/uis/Button';
+import RNFetchBlob from 'rn-fetch-blob';
+import type {ReactElement} from 'react';
+import Voice, {
+  SpeechRecognizedEvent,
+  SpeechResultsEvent,
+  SpeechErrorEvent,
+} from '@react-native-voice/voice';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+const styles: any = StyleSheet.create({
+  stat: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  container: {
+    flex: 1,
+    backgroundColor: '#455A64',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
-  sectionDescription: {
+  titleTxt: {
+    marginTop: 100,
+    color: 'white',
+    fontSize: 28,
+  },
+  viewRecorder: {
+    marginTop: 40,
+    width: '100%',
+    alignItems: 'center',
+  },
+  recordBtnWrapper: {
+    flexWrap: 'wrap',
+    gap: 20,
+  },
+  viewPlayer: {
+    marginTop: 60,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  viewBarWrapper: {
+    marginTop: 28,
+    marginHorizontal: 28,
+    alignSelf: 'stretch',
+  },
+  viewBar: {
+    backgroundColor: '#ccc',
+    height: 4,
+    alignSelf: 'stretch',
+  },
+  viewBarPlay: {
+    backgroundColor: 'white',
+    height: 4,
+    width: 0,
+  },
+  playStatusTxt: {
     marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+    color: '#ccc',
   },
-  highlight: {
-    fontWeight: '700',
+  playBtnWrapper: {
+    flexDirection: 'row',
+    marginTop: 40,
+  },
+  btn: {
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  txt: {
+    color: 'white',
+    fontSize: 14,
+    marginHorizontal: 8,
+    marginVertical: 4,
+  },
+  txtRecordCounter: {
+    marginTop: 32,
+    color: 'white',
+    fontSize: 20,
+    textAlignVertical: 'center',
+    fontWeight: '200',
+    fontFamily: 'Helvetica Neue',
+    letterSpacing: 3,
+  },
+  txtCounter: {
+    marginTop: 12,
+    color: 'white',
+    fontSize: 20,
+    textAlignVertical: 'center',
+    fontWeight: '200',
+    fontFamily: 'Helvetica Neue',
+    letterSpacing: 3,
   },
 });
+
+interface State {
+  isLoggingIn: boolean;
+  recordSecs: number;
+  recordTime: string;
+  currentPositionSec: number;
+  currentDurationSec: number;
+  playTime: string;
+  duration: string;
+  recognized: string;
+  pitch: string;
+  error: string;
+  end: string;
+  started: string;
+  results: any;
+  partialResults: any;
+}
+
+const screenWidth = Dimensions.get('screen').width;
+
+class App extends Component<any, State> {
+  private dirs = RNFetchBlob.fs.dirs;
+  private path = Platform.select({
+    ios: undefined,
+    android: undefined,
+
+    // Discussion: https://github.com/hyochan/react-native-audio-recorder-player/discussions/479
+    // ios: 'https://firebasestorage.googleapis.com/v0/b/cooni-ebee8.appspot.com/o/test-audio.mp3?alt=media&token=d05a2150-2e52-4a2e-9c8c-d906450be20b',
+    // ios: 'https://staging.media.ensembl.fr/original/uploads/26403543-c7d0-4d44-82c2-eb8364c614d0',
+    // ios: 'hello.m4a',
+    // android: `${this.dirs.CacheDir}/hello.mp3`,
+  });
+
+  private audioRecorderPlayer: AudioRecorderPlayer;
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      isLoggingIn: false,
+      recordSecs: 0,
+      recordTime: '00:00:00',
+      currentPositionSec: 0,
+      currentDurationSec: 0,
+      playTime: '00:00:00',
+      duration: '00:00:00',
+      recognized: '',
+      pitch: '',
+      error: '',
+      end: '',
+      started: '',
+      results: [],
+      partialResults: [],
+    };
+
+    this.audioRecorderPlayer = new AudioRecorderPlayer();
+    this.audioRecorderPlayer.setSubscriptionDuration(0.1); // optional. Default is 0.5
+    Voice.onSpeechStart = this.onSpeechStart;
+    Voice.onSpeechRecognized = this.onSpeechRecognized;
+    Voice.onSpeechEnd = this.onSpeechEnd;
+    Voice.onSpeechError = this.onSpeechError;
+    Voice.onSpeechResults = this.onSpeechResults;
+    Voice.onSpeechPartialResults = this.onSpeechPartialResults;
+    Voice.onSpeechVolumeChanged = this.onSpeechVolumeChanged;
+  }
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+  onSpeechStart = (e: any) => {
+    console.log('onSpeechStart: ', e);
+    this.setState({
+      started: '√',
+    });
+  };
+
+  onSpeechRecognized = (e: SpeechRecognizedEvent) => {
+    console.log('onSpeechRecognized: ', e);
+    this.setState({
+      recognized: '√',
+    });
+  };
+
+  onSpeechEnd = (e: any) => {
+    console.log('onSpeechEnd: ', e);
+    this.setState({
+      end: '√',
+    });
+  };
+
+  onSpeechError = (e: SpeechErrorEvent) => {
+    console.log('onSpeechError: ', e);
+    this.setState({
+      error: JSON.stringify(e.error),
+    });
+  };
+
+  onSpeechResults = (e: SpeechResultsEvent) => {
+    console.log('onSpeechResults: ', e);
+    this.setState({
+      results: e.value,
+    });
+  };
+
+  onSpeechPartialResults = (e: SpeechResultsEvent) => {
+    console.log('onSpeechPartialResults: ', e);
+    this.setState({
+      partialResults: e.value,
+    });
+  };
+
+  onSpeechVolumeChanged = (e: any) => {
+    console.log('onSpeechVolumeChanged: ', e);
+    this.setState({
+      pitch: e.value,
+    });
+  };
+
+  _startRecognizing = async () => {
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _stopRecognizing = async () => {
+    try {
+      await Voice.stop();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _cancelRecognizing = async () => {
+    try {
+      await Voice.cancel();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  _destroyRecognizer = async () => {
+    try {
+      await Voice.destroy();
+    } catch (e) {
+      console.error(e);
+    }
+    this.setState({
+      recognized: '',
+      pitch: '',
+      error: '',
+      started: '',
+      results: [],
+      partialResults: [],
+      end: '',
+    });
+  };
+
+  public render(): ReactElement {
+    let playWidth =
+      (this.state.currentPositionSec / this.state.currentDurationSec) *
+      (screenWidth - 56);
+
+    if (!playWidth) {
+      playWidth = 0;
+    }
+
+    return (
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <Text style={styles.titleTxt}>Ses Kayıt ve Tanıma Uygulaması</Text>
+          <Text style={styles.txtRecordCounter}>{this.state.recordTime}</Text>
+          <View style={styles.viewRecorder}>
+            <View style={styles.recordBtnWrapper}>
+              <Button
+                style={styles.btn}
+                onPress={this.onStartRecord}
+                textStyle={styles.txt}>
+                Kaydı Başlat & Tanımla
+              </Button>
+              <Button
+                style={[
+                  styles.btn,
+                  {
+                    marginLeft: 12,
+                  },
+                ]}
+                onPress={this.onPauseRecord}
+                textStyle={styles.txt}>
+                Kaydı & Tanımlamayı Duraklat
+              </Button>
+              <Button
+                style={[
+                  styles.btn,
+                  {
+                    marginLeft: 12,
+                  },
+                ]}
+                onPress={this.onResumeRecord}
+                textStyle={styles.txt}>
+                Kaydı Sürdür
+              </Button>
+              <Button
+                style={[styles.btn, {marginLeft: 12}]}
+                onPress={this.onStopRecord}
+                textStyle={styles.txt}>
+                Kaydı & Tanımlamayı Durdur
+              </Button>
+            </View>
+          </View>
+          <View style={styles.viewPlayer}>
+            <TouchableOpacity
+              style={styles.viewBarWrapper}
+              onPress={this.onStatusPress}>
+              <View style={styles.viewBar}>
+                <View style={[styles.viewBarPlay, {width: playWidth}]} />
+              </View>
+            </TouchableOpacity>
+            <Text style={styles.txtCounter}>
+              {this.state.playTime} / {this.state.duration}
+            </Text>
+            <View style={styles.playBtnWrapper}>
+              <Button
+                style={styles.btn}
+                onPress={this.onStartPlay}
+                textStyle={styles.txt}>
+                Oynat
+              </Button>
+              <Button
+                style={[
+                  styles.btn,
+                  {
+                    marginLeft: 12,
+                  },
+                ]}
+                onPress={this.onPausePlay}
+                textStyle={styles.txt}>
+                Duraklat
+              </Button>
+              <Button
+                style={[
+                  styles.btn,
+                  {
+                    marginLeft: 12,
+                  },
+                ]}
+                onPress={this.onResumePlay}
+                textStyle={styles.txt}>
+                Sürdür
+              </Button>
+              <Button
+                style={[
+                  styles.btn,
+                  {
+                    marginLeft: 12,
+                  },
+                ]}
+                onPress={this.onStopPlay}
+                textStyle={styles.txt}>
+                Durdur
+              </Button>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.stat}>Kısmi Sonuçlar</Text>
+            {this.state.partialResults.map((result: any, index: number) => {
+              return (
+                <Text key={`partial-result-${index}`} style={styles.stat}>
+                  {result}
+                </Text>
+              );
+            })}
+          </View>
+          <View>
+            <Text style={styles.stat}>Sonuçlar</Text>
+            {this.state.results.map((result: any, index: any) => {
+              return (
+                <Text key={`sonuc-${index}`} style={styles.stat}>
+                  {result}
+                </Text>
+              );
+            })}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+
+  private onStatusPress = (e: any): void => {
+    const touchX = e.nativeEvent.locationX;
+    console.log(`touchX: ${touchX}`);
+
+    const playWidth =
+      (this.state.currentPositionSec / this.state.currentDurationSec) *
+      (screenWidth - 56);
+    console.log(`currentPlayWidth: ${playWidth}`);
+
+    const currentPosition = Math.round(this.state.currentPositionSec);
+
+    if (playWidth && playWidth < touchX) {
+      const addSecs = Math.round(currentPosition + 1000);
+      this.audioRecorderPlayer.seekToPlayer(addSecs);
+      console.log(`addSecs: ${addSecs}`);
+    } else {
+      const subSecs = Math.round(currentPosition - 1000);
+      this.audioRecorderPlayer.seekToPlayer(subSecs);
+      console.log(`subSecs: ${subSecs}`);
+    }
+  };
+
+  private onStartRecord = async (): Promise<void> => {
+    this._startRecognizing();
+    if (Platform.OS === 'android') {
+      try {
+        this.onSpeechStart(this);
+        const grants = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        ]);
+
+        console.log('Dahili Hafızıa Yazım İzni Alındı', grants);
+
+        if (
+          grants['android.permission.WRITE_EXTERNAL_STORAGE'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          grants['android.permission.READ_EXTERNAL_STORAGE'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          grants['android.permission.RECORD_AUDIO'] ===
+            PermissionsAndroid.RESULTS.GRANTED
+        ) {
+          console.log('İzinler Alındı');
+        } else {
+          console.log('İzinler Alınamadı');
+
+          return;
+        }
+      } catch (err) {
+        console.warn(err);
+
+        return;
+      }
+    }
+
+    const audioSet: AudioSet = {
+      AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+      AudioSourceAndroid: AudioSourceAndroidType.MIC,
+      AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+      AVNumberOfChannelsKeyIOS: 2,
+      AVFormatIDKeyIOS: AVEncodingOption.aac,
+      OutputFormatAndroid: OutputFormatAndroidType.AAC_ADTS,
+    };
+
+    console.log('audioSet', audioSet);
+
+    const uri = await this.audioRecorderPlayer.startRecorder(
+      this.path,
+      audioSet,
+    );
+
+    this.audioRecorderPlayer.addRecordBackListener((e: RecordBackType) => {
+      this.setState({
+        recordSecs: e.currentPosition,
+        recordTime: this.audioRecorderPlayer.mmssss(
+          Math.floor(e.currentPosition),
+        ),
+      });
+    });
+    console.log(`uri: ${uri}`);
+  };
+
+  private onPauseRecord = async (): Promise<void> => {
+    try {
+      this._stopRecognizing();
+      const r = await this.audioRecorderPlayer.pauseRecorder();
+      console.log(r);
+    } catch (err) {
+      console.log('pauseRecord', err);
+    }
+  };
+
+  private onResumeRecord = async (): Promise<void> => {
+    await this.audioRecorderPlayer.resumeRecorder();
+  };
+
+  private onStopRecord = async (): Promise<void> => {
+    this._stopRecognizing();
+    const result = await this.audioRecorderPlayer.stopRecorder();
+    // await Voice.stop();
+    this.audioRecorderPlayer.removeRecordBackListener();
+    this.setState({
+      recordSecs: 0,
+    });
+    console.log(result);
+  };
+
+  private onStartPlay = async (): Promise<void> => {
+    console.log('onStartPlay', this.path);
+
+    try {
+      const msg = await this.audioRecorderPlayer.startPlayer(this.path);
+
+      //? Default path
+      // const msg = await this.audioRecorderPlayer.startPlayer();
+      const volume = await this.audioRecorderPlayer.setVolume(1.0);
+      console.log(`path: ${msg}`, `volume: ${volume}`);
+
+      this.audioRecorderPlayer.addPlayBackListener((e: PlayBackType) => {
+        console.log('playBackListener', e);
+        this.setState({
+          currentPositionSec: e.currentPosition,
+          currentDurationSec: e.duration,
+          playTime: this.audioRecorderPlayer.mmssss(
+            Math.floor(e.currentPosition),
+          ),
+          duration: this.audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+        });
+      });
+    } catch (err) {
+      console.log('startPlayer error', err);
+    }
+  };
+
+  private onPausePlay = async (): Promise<void> => {
+    await this.audioRecorderPlayer.pausePlayer();
+  };
+
+  private onResumePlay = async (): Promise<void> => {
+    await this.audioRecorderPlayer.resumePlayer();
+  };
+
+  private onStopPlay = async (): Promise<void> => {
+    console.log('onStopPlay');
+    this.audioRecorderPlayer.stopPlayer();
+    this.audioRecorderPlayer.removePlayBackListener();
+  };
+}
 
 export default App;
